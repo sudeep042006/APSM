1794c678ff51a9bfbc25d58b4f5dc1968b603b23
sudeep042006 <sudeepkuralkar02@gmail.com>
2026-06-12 23:02:38 +0530

    Remove legacy analytics modules

--- backend/modules/analytics/insta-analytics/insta.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getInstagramAnalytics } from './insta.controller.js';
-import { requireAuth } from '../../../middleware/auth.js';
-
-const router = express.Router();
-
-router.get('/', requireAuth, getInstagramAnalytics);
-
-export default router;
